c8db4d7224a2e458bcbbdb99240c1d8a91f331fe
Robert Roibu <roibu1@umbc.edu>
2021-06-23 16:54:24 -0400

    Update main.py
    can now print the map

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 import pygame
-
+ 
 FPS = 60
 LEVEL = []
 BACKGROUND_COLOR = ("black")
 WIDTH,HEIGHT = 900,500
-WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #window
+screen = pygame.display.set_mode((WIDTH,HEIGHT)) #window
 pygame.display.set_caption("Pac-Man")
-
+ 
 BLOCK_SIZE = 32
-
+ 
 char_to_image = {
     '.': 'dot.png',
     '=': 'wall.png',
     '*': 'power.png',
 }
-
+ 
 def loadMap(number): #load in the text file into the array LEVEL 
     file = "map-%s.txt" % number
     with open(file) as f:
         for line in f:
             row = []
-            for block in line.strip():
-                row.append(block)
+            for char in line.strip():
+                if char != " ":
+                    screen_char = pygame.image.load(char_to_image[char])
+                    row.append(screen_char)
+                else:
+                    row.append(0)
             LEVEL.append(row)
-
-    
+ 
+    for row in LEVEL:
+        for char in row:
+            print(char)
     
 def draw(number): # trying to use blit to take the char array and change it to string
-    file = "map-%s.txt" % number
-    col,row = 0,0
-    with open(file, "r") as f:
-        for line in f:
-            for char in line:
-                screen_char = pygame.image.load(char_to_image[char])
-                WIN.blit(screen_char, (col, row))
-                col += 1
-        row += 1
-
+    x = y = 0
+    for row in LEVEL:
+        for char in row:
+            if char != 0:
+                screen.blit(char, (x,y))
+            x += 32
+        x = 0
+        y += 32
+ 
     pygame.display.update()
-
+ 
 def main():
-
+ 
     loadMap(1) # change the number to change the map
    
     clock = pygame.time.Clock()
@@ -52,11 +57,11 @@ def main():
                 play = False
         draw(1)
         
-    #testing github link
+    
     
     pygame.quit()
-
-
-
+ 
+ 
+ 
 if __name__ == "__main__":
     main()
